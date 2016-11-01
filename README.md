99dde7adb6578c45943cdc440b21c437ded20cea
Juan Riaza <github@juanriaza.com>
2016-11-01 19:39:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Juan Riaza website
+
+A Pelican (pelican link) based blog, design based on https://github.com/poole/hyde.
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Juan Riaza'
+SITENAME = 'Juan Riaza'
+SITEURL = ''
+
+THEME = 'theme'
+PATH = 'content'
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+SOCIAL = (
+    ('email', 'juanriaza@gmail.com'),
+    ('twitter', 'https://twitter.com/juanriaza'),
+    ('linkedin', 'https://es.linkedin.com/in/juanriaza'),
+    ('github', 'https://github.com/juanriaza'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+BIO = "A Software Engineer based in Spain. Loving Python, Django, Web Scraping and complex data pipelines."
+PROFILE_IMAGE = "profile_pic.jpg"
